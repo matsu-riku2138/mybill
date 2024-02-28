915224c741bb169d3e551dc82ed0445a43570e87
maritsuku <61532854+matsu-riku2138@users.noreply.github.com>
2024-02-28 22:46:40 +0900

    URLの修正

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   build:{
     transpileDependencies: true,
-    publicPath: process.env.NODE_ENV === 'production' ? '/product-frontend/' : '/',
+    publicPath: '/mybill',
     outDir: './docs',
   },
   plugins: [
